8fddf8b63a8e89dea37e8e7293cb6930bc63078b
Alex <alex.williams1@my.jcu.edu.au>
2024-06-15 22:50:01 +1000

    Add randoms program

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+import random
+
+print(random.randint(5, 20))  # 14
+# the smallest possible value is 5.
+# the largest possible value was 20
+
+print(random.randrange(3, 10, 2))  # 7
+# smallest possible number is 3
+# Largest possible number is 9
+
+print(random.uniform(2.5, 5.5))  # 3.696444539925139
+# The smallest possible value is 2.5
+# The largest possible value is 5.499999999999999
+
+print(random.randint(1, 100))
