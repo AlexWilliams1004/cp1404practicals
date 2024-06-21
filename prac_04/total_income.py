43221cbea03972860c5fd621a958d9e7a392d50c
Alex <alex.williams1@my.jcu.edu.au>
2024-06-21 11:42:23 +1000

    refactor total income program into functions

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -1,24 +1,37 @@
 """
 CP1404/CP5632 Practical
-Starter code for cumulative total income program
+Updated cumulative total income program
+Alex Williams
 """
 
 
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    print_report_header()
+    report_totals = add_report_total(incomes, number_of_months)
+    for report_total in report_totals:
+        print(report_total)
+
+
+def print_report_header():
     print("\nIncome Report\n-------------")
+
+
+def add_report_total(incomes, number_of_months):
+    totals = []
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        totals.append("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+    return totals
 
 
 main()
