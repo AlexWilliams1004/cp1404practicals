be22cd1e779ce3da4c53af222383eb3b65a0febd
Alex <alex.williams1@my.jcu.edu.au>
2024-08-04 11:28:00 +1000

    Add taxi programs

---
+++ prac_09/taxi_text.py
@@ -0,0 +1,16 @@
+from prac_09.taxi import Taxi
+
+
+def main():
+    """Create new taxi object."""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(f"{my_taxi}\nCurrent fare: ${my_taxi.get_fare():.2f}")
+
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(f"{my_taxi}\nCurrent fare: ${my_taxi.get_fare():.2f}")
+
+
+if __name__ == "__main__":
+    main()
