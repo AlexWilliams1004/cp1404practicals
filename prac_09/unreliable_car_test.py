a25e918791fd086174081d848ce95d7fcfbe717e
Alex <alex.williams1@my.jcu.edu.au>
2024-08-04 14:33:40 +1000

    Add unreliable car programs

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,18 @@
+"""
+Unreliable Car Test - CP1404 Practical 9
+Alex Williams
+"""
+
+from unreliable_car import UnreliableCar
+
+
+def main():
+    unreliable_car = UnreliableCar("Hilux", 100, 50)
+    for i in range(10):
+        distance_driven = unreliable_car.drive(20)
+        print(
+            f"Attempt {i + 1}: Drove {distance_driven}km, {unreliable_car.odometer}, Fuel left: {unreliable_car.fuel}")
+
+
+if __name__ == "__main__":
+    main()
