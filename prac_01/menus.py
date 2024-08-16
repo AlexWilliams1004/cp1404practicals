46e70efd4091bb30b1ed37f9deac26620fb1d8a6
AlexWilliams1004 <alex.williams1004@gmail.com>
2024-08-16 14:06:00 +1000

    Change MENU to menu and capitalise greetings

--- prac_01/menus.py
+++ prac_01/menus.py
@@ -1,5 +1,7 @@
 """
-Menus
+Menus - CP1404 Practicals
+
+    - Alex Williams 16.08.2024 -
 
 Pseudocode:
 get name
@@ -16,18 +18,19 @@ while choice != Q
    get choice
 print finished message
 """
-
-MENU = "(H)ello\n(G)oodbye\n(Q)uit"
-name = input("Enter name: ")
-print(MENU)
+menu = """(H)ello
+(G)oodbye
+(Q)uit"""
+name = input("Enter name: ").title()
+print(menu)
 choice = input(">>> ").upper()
 while choice != "Q":
     if choice == "H":
-        print(f"hello {name}")
+        print(f"Hello {name}")
     elif choice == "G":
-        print(f"goodbye {name}")
+        print(f"Goodbye {name}")
     else:
-        print("invalid message")
-    print(MENU)
+        print("Invalid message")
+    print(menu)
     choice = input(">>> ").upper()
 print("Finished.")
