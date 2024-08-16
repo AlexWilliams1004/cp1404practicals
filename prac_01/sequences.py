86e22bf923bacee1708cee543d6e18d3418da0b6
AlexWilliams1004 <alex.williams1004@gmail.com>
2024-08-16 14:44:25 +1000

    Refactor calculations into fuctions

--- prac_01/sequences.py
+++ prac_01/sequences.py
@@ -9,26 +9,43 @@ MENU = """1. Show the even numbers from x to y
 3. Show the squares of the numbers from x to y (e.g., if x, y = 2, 4 then: 4 9 16)
 4. Exit the program"""
 
-x = int(input("x: "))
-y = int(input("y: "))
 
-print(MENU)
-choice = input(">>> ")
-while choice != "4":
-    if choice == "1":
-        for i in range(x + x % 2, y + 1, 2):
-            print(i, end=' ')
-    elif choice == "2":
-        if x % 2 != 0:
-            for i in range(x, y + 1, 2):
-                print(i, end=' ')
-        else:
-            for i in range(x + 1, y + 1, 2):
-                print(i, end=' ')
-    else:
-        for i in range(x, y + 1):
-            print(i ** 2, end=' ')
-    print()
+def main():
+    x = int(input("x: "))
+    y = int(input("y: "))
     print(MENU)
     choice = input(">>> ")
-print("Program finished")
+    while choice != "4":
+        if choice == "1":
+            display_even_numbers(x, y)
+        elif choice == "2":
+            display_odd_numbers(x, y)
+        else:
+            display_numbers_squared(x, y)
+        print()
+        print(MENU)
+        choice = input(">>> ")
+    print("Program finished")
+
+
+def display_even_numbers(x, y):
+    for i in range(x + x % 2, y + 1, 2):
+        print(i, end=' ')
+
+
+def display_odd_numbers(x, y):
+    if x % 2 != 0:
+        for i in range(x, y + 1, 2):
+            print(i, end=' ')
+    else:
+        for i in range(x + 1, y + 1, 2):
+            print(i, end=' ')
+
+
+def display_numbers_squared(x, y):
+    for i in range(x, y + 1):
+        print(i ** 2, end=' ')
+
+
+if __name__ == '__main__':
+    main()
