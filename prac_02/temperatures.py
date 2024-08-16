44e13d1b1343fbe25b615c7626ff846871b452d2
AlexWilliams1004 <alex.williams1004@gmail.com>
2024-08-16 15:07:03 +1000

    Add module docstring and function docstring comments

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,12 +1,17 @@
 """
-CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
+Temperature Conversion - CP1404/CP5632 Practicals
+
+    - Alex Williams 16.08.2024 -
+
 """
 
-MENU = "(C) - Convert Celsius to Fahrenheit\n(F) - Convert Fahrenheit to Celsius\n(Q) - Quit"
+MENU = """(C) - Convert Celsius to Fahrenheit
+(F) - Convert Fahrenheit to Celsius
+(Q) - Quit"""
 
 
 def main():
+    """Main program."""
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
@@ -26,11 +31,13 @@ def main():
 
 
 def convert_fahrenheit_to_celsius(fahrenheit):
+    """Convert fahrenheit to celsius."""
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
 
 
 def convert_celsius_to_fahrenheit(celsius):
+    """Convert celsius to fahrenheit."""
     fahrenheit = celsius * 9.0 / 5 + 32
     return fahrenheit
 
