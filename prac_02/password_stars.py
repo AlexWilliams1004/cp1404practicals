4d7a09db1d25122576c2e9189be574ac7e6610ff
AlexWilliams1004 <alex.williams1004@gmail.com>
2024-08-16 15:03:34 +1000

    Add module docstring and function docstring comments

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,19 +1,30 @@
+"""
+Password Stars - CP1404 Practicals
+
+    - Alex Williams 16.08.2024 -
+
+"""
+
+
 def main():
+    """Main function."""
     password_length = 10
     password = get_valid_password(password_length)
     print_password(password)
 
 
-def print_password(password):
-    print(f"{'*' * len(password)}")
-
-
 def get_valid_password(password_length):
+    """Get a valid password length."""
     password = input("Enter your password: ")
     while len(password) < password_length:
         print(f"Password must be at least {password_length} long.")
         password = input("Enter your password: ")
     return password
+
+
+def print_password(password):
+    """Print stars of password length."""
+    print(f"{'*' * len(password)}")
 
 
 main()
