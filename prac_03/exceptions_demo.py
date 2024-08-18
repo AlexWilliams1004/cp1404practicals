6f530aa3ada4f78ea380261fb485dc61a4c064d5
AlexWilliams1004 <alex.williams1004@gmail.com>
2024-08-18 16:19:07 +1000

    Add signed module docstring and error checking to remove 0 as possible denominator input

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -1,21 +1,26 @@
 """
-CP1404/CP5632 - Practical
+Exceptions - CP1404 Practicals
+
 Answer the following questions:
 1. When will a ValueError occur?
+    - In this case, when the inputs are not integers.
 2. When will a ZeroDivisionError occur?
+    - In this case, when the denominator input is zero.
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+    - Yes.
+
+    - Alex Williams 18-08.2024 -
+
 """
 
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
     while denominator == 0:
-        print("Invalid input")
+        print("Cannot divide by zero!")
         denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
-except ValueError:  # if not integer
+except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:  # if denominator is zero
-    print("Cannot divide by zero!")
 print("Finished.")
