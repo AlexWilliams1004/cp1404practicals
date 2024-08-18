1c771e0a8887c0009657eaaa0dee18aa61e65dcc
AlexWilliams1004 <alex.williams1004@gmail.com>
2024-08-18 11:34:45 +1000

    Add module docstring comment to randoms program

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,15 +1,20 @@
+"""
+Randoms - CP1404 Practicals
+
+    - Alex Williams 18.08.2024 -
+
+"""
+
 import random
 
-print(random.randint(5, 20))  # 14
-# the smallest possible value is 5.
-# the largest possible value was 20
+print(random.randint(5, 20))  # line 1
+# smallest number should be 5, largest should be 20
 
-print(random.randrange(3, 10, 2))  # 7
-# smallest possible number is 3
-# Largest possible number is 9
+print(random.randrange(3, 10, 2))  # line 2
+# smallest number should be 3, largest should be 9
 
-print(random.uniform(2.5, 5.5))  # 3.696444539925139
-# The smallest possible value is 2.5
-# The largest possible value is 5.499999999999999
+print(random.uniform(2.5, 5.5))  # line  3
+# smallest number should be 2.5, largest should be 5.5
 
 print(random.randint(1, 100))
+# smallest number: 1, largest number: 100
