65d727a2db6d6f358b2ffab35339b6bddf1d36d1
AlexWilliams1004 <alex.williams1004@gmail.com>
2024-08-18 16:57:17 +1000

    Add signature and date to module docstring and label each exercise as instructed in the practical

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,26 +1,29 @@
 """
-files
+Files - CP1404 Practicals
+
+    - Alex Williams 18.08.2024 -
+
 """
 
-# Program 1
+# 1
 out_file = open("name.txt", 'w')
 name = input("name: ")
 print(name, file=out_file)
 out_file.close()
 
-# Program 2
+# 2
 in_file = open("name.txt", 'r')
 name = in_file.read().strip()
 in_file.close()
-print(f"Your name is {name}")
+print(f"Hi {name}!")
 
-# Program 3
+# 3
 with open("numbers.txt", 'r') as in_file:
     first_number = int(in_file.readline())
     second_number = int(in_file.readline())
 print(first_number + second_number)
 
-# Program 4
+# 4
 with open("numbers.txt", 'r') as in_file:
     total = 0
     for line in in_file:
